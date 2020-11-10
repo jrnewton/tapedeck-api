c066fe306dd79f790e93c317cc385836cd56ce3e
Jon Newton <newt@hey.com>
2020-11-10 11:51:22 -0500

    Hook up the form to a basic model and disable second form not currently used.

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 'use strict';
 
-const app = Vue.create({});
+const app = Vue.createApp({
+  data() {
+    return {
+      archiveURL: '',
+      archiveURLMessage: ''
+    };
+  },
+  methods: {
+    archiveSubmit(event) {
+      alert(`Recording ${this.archiveURL} now!`);
+      this.archiveURL = '';
+    }
+  },
+  computed: {}
+});
 
 const mounted = app.mount('#tape-deck-controls');
