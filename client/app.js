40012a4f6f530a9a56d09c8126d0d0df567af906
Jon Newton <newt@hey.com>
2020-11-11 09:26:09 -0500

    Add some manual validation. Yuck.  Will use vuelidate next.

--- client/app.js
+++ client/app.js
@@ -3,13 +3,27 @@
 const app = Vue.createApp({
   data() {
     return {
+      email: '',
       archiveURL: '',
-      archiveURLMessage: ''
+      status: ''
     };
   },
   methods: {
     archiveSubmit(event) {
-      alert(`Recording ${this.archiveURL} now!`);
+      if (this.email === '') {
+        alert('Please enter your email address');
+        return;
+      }
+
+      if (this.archiveURL === '') {
+        alert('Please enter a URL to archive');
+        return;
+      }
+
+      //TODO: call faas
+      alert(
+        `Thanks! We'll email you at ${this.email} when the archive is available`
+      );
       this.archiveURL = '';
     }
   },
