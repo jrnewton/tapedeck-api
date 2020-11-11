2921a1c8788b6cbec5a871f79653898aacab38c8
Jon Newton <newt@hey.com>
2020-11-11 09:26:28 -0500

    Starting code for S3 connectivity based on aws example.

--- service/archive.js
+++ service/archive.js
@@ -1,6 +1,23 @@
 'use strict';
 
 const express = require('express');
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
+
+const awsCreds = new AWS.CognitoIdentityCredentials({
+  IdentityPoolId: 'IDENTITY_POOL_ID'
+});
+const awsConfig = new AWS.Config({
+  credentials: awsCreds,
+  region: 'us-east-2'
+});
+
+// AWS.config.update({
+//   accessKeyId: 'accessKeyId',
+//   secretAccessKey: 'secretAccessKey',
+//   timeout: 15000
+// });
+
 const app = express();
 
 app.post('/archive', (req, res) => {
@@ -8,6 +25,24 @@ app.post('/archive', (req, res) => {
   if (url === '') {
     res.status(404).json("{ 'message': 'missing url' }").end();
   } else {
+    const s3 = new AWS.S3();
+
+    //bucket should be per user and each object is an mp3 file
+    const bucketName = 'tapedeck-' + email + uuid.v4();
+    const keyName = 'hello_world.txt';
+
+    s3.createBucket({ Bucket: bucketName }, function () {
+      const params = { Bucket: bucketName, Key: keyName, Body: 'Hello World!' };
+      s3.putObject(params, function (err, data) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(
+            'Successfully uploaded data to ' + bucketName + '/' + keyName
+          );
+        }
+      });
+    });
   }
 });
 
