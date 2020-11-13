647d37b739d29d3aaffd063820e3767f8b91867d
Jon Newton <newt@hey.com>
2020-11-13 16:26:30 -0500

    Hello world S3 example

--- service/archive.js
+++ service/archive.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+const uuid = require('uuid');
 
 AWS.config.getCredentials(function (err) {
   if (err) {
@@ -12,3 +13,38 @@ AWS.config.getCredentials(function (err) {
 });
 
 console.log('Region: ', AWS.config.region);
+
+// Create unique bucket name
+const bucketName = 'node-sdk-sample-' + uuid.v4();
+// Create name for uploaded object key
+const keyName = 'hello_world.txt';
+
+// Create a promise on S3 service object
+const bucketPromise = new AWS.S3({ apiVersion: '2006-03-01' })
+  .createBucket({ Bucket: bucketName })
+  .promise();
+
+// Handle promise fulfilled/rejected states
+bucketPromise
+  .then(function (data) {
+    // Create params for putObject call
+    const objectParams = {
+      Bucket: bucketName,
+      Key: keyName,
+      Body: 'Hello World!'
+    };
+
+    // Create object upload promise
+    const uploadPromise = new AWS.S3({ apiVersion: '2006-03-01' })
+      .putObject(objectParams)
+      .promise();
+
+    uploadPromise.then(function (data) {
+      console.log(
+        'Successfully uploaded data to ' + bucketName + '/' + keyName
+      );
+    });
+  })
+  .catch(function (err) {
+    console.error(err, err.stack);
+  });
