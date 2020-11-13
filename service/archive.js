24b43eb5572550c8e7f4d71b0a2e455317dbc009
Jon Newton <newt@hey.com>
2020-11-13 15:33:11 -0500

    Beginning hello world of S3 upload

--- service/archive.js
+++ service/archive.js
@@ -1,52 +1,18 @@
 'use strict';
 
-const express = require('express');
 const AWS = require('aws-sdk');
-const uuid = require('uuid');
 
-const awsCreds = new AWS.CognitoIdentityCredentials({
-  IdentityPoolId: 'IDENTITY_POOL_ID'
-});
-const awsConfig = new AWS.Config({
-  credentials: awsCreds,
-  region: 'us-east-2'
+const credentials = new AWS.SharedIniFileCredentials({
+  profile: process.env['AWS_PROFILE']
 });
 
-// AWS.config.update({
-//   accessKeyId: 'accessKeyId',
-//   secretAccessKey: 'secretAccessKey',
-//   timeout: 15000
-// });
+AWS.config.credentials = credentials;
 
-const app = express();
-
-app.post('/archive', (req, res) => {
-  const url = req.body.url;
-  if (url === '') {
-    res.status(404).json("{ 'message': 'missing url' }").end();
+AWS.config.getCredentials(function (err) {
+  if (err) {
+    console.log('credentials not loaded');
+    console.log(err.stack);
   } else {
-    const s3 = new AWS.S3();
-
-    //bucket should be per user and each object is an mp3 file
-    const bucketName = 'tapedeck-' + email + uuid.v4();
-    const keyName = 'hello_world.txt';
-
-    s3.createBucket({ Bucket: bucketName }, function () {
-      const params = { Bucket: bucketName, Key: keyName, Body: 'Hello World!' };
-      s3.putObject(params, function (err, data) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(
-            'Successfully uploaded data to ' + bucketName + '/' + keyName
-          );
-        }
-      });
-    });
+    console.log('Access key:', AWS.config.credentials.accessKeyId);
   }
-});
-
-const port = 9002;
-app.listen(port, () => {
-  console.log(`Express running on port ${port}`);
 });
