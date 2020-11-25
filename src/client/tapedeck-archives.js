a7f5eb02cd17a71ebb4910854a6b709f4fa42095
Jon Newton <newt@hey.com>
2020-11-25 13:42:37 -0500

    Cleanup from audio player experiments.
    Move that code to a branch.  Keep main simple.
    
    Split archive list and playback into separate pages (although will probably merge back into SPA)

--- src/client/tapedeck-archives.js
+++ src/client/tapedeck-archives.js
@@ -6,43 +6,13 @@ const app = Vue.createApp({
       email: 'newt@hey.com',
       recent: [
         {
-          muted: false,
+          id: 1,
           desc: 'Test File',
           url:
             'https://tapedeck-88da7a2d-990c-474b-aa2f-2716203101d9.s3.us-east-2.amazonaws.com/test.mp3'
         }
-      ],
-      status: ''
+      ]
     };
-  },
-  methods: {
-    async play(item) {
-      try {
-        await this.$refs.audio.play();
-      } catch (error) {
-        console.log('error playing', JSON.stringify(error));
-      }
-    },
-    async pause(item) {
-      try {
-        await this.$refs.audio.pause();
-      } catch (error) {
-        console.log('error playing', JSON.stringify(error));
-      }
-    },
-    forward(item) {
-      this.$refs.audio.currentTime = this.$refs.audio.currentTime + 15;
-    },
-    mute(item) {
-      item.muted = !item.muted;
-    },
-    muteAction(item) {
-      if (item.muted) {
-        return 'Unmute';
-      } else {
-        return 'Mute';
-      }
-    }
   }
 });
 
