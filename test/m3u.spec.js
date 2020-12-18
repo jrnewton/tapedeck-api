ff93f4805694413c75ed8283e8f8b2d550040206
Jon Newton <newt@hey.com>
2020-12-18 11:32:54 -0500

    Reapply mocha change after revert.

--- test/m3u.spec.js
+++ test/m3u.spec.js
@@ -1,30 +1,30 @@
 'use strict';
 
 const fs = require('fs');
-const test = require('ava');
+const assert = require('assert');
 const m3u = require('./../src/function/m3u');
 
-test('simple parse test', (t) => {
+it('should parse a simple m3u playlist without error', () => {
   const fileContents = fs.readFileSync(__dirname + '/test.m3u', 'utf-8');
   const contents = m3u.parse(fileContents);
   console.log(contents);
 
-  t.is(contents.length, 2);
-  t.is(contents[0].title, 'test');
-  t.is(
+  assert.strictEqual(contents.length, 2);
+  assert.strictEqual(contents[0].title, 'test');
+  assert.strictEqual(
     contents[0].uri,
     'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3'
   );
 });
 
-test('getMp3URIs test', (t) => {
+it('should provide a list of files from the m3u playlist', () => {
   const fileContents = fs.readFileSync(__dirname + '/test.m3u', 'utf-8');
   const contents = m3u.getMP3URIs(fileContents);
   console.log(contents);
 
-  t.is(contents.length, 1);
-  t.is(contents[0].title, 'test');
-  t.is(
+  assert.strictEqual(contents.length, 1);
+  assert.strictEqual(contents[0].title, 'test');
+  assert.strictEqual(
     contents[0].uri,
     'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3'
   );
