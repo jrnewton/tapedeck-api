f46dda5543c01b4abeb74359598f4acd113bd372
Jon Newton <newt@hey.com>
2020-12-23 14:52:54 -0500

    Rearrange file structure again to get aws-sam and circleci working.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,12 @@
+'use strict';
+
 module.exports = {
   env: {
     commonjs: true,
-    es2018: true,
+    es2020: true,
     node: true,
     mocha: true
   },
-  /* eslint:recommended property enables rules that report common problems 
-       See the list at https://eslint.org/docs/rules/ */
-  /* eslint/prettier integration read https://bit.ly/3kQFe3u */
   extends: ['eslint:recommended', 'prettier'],
   rules: {
     'strict': 'error',
