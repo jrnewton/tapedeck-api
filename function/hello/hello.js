bcb185839557fc86db12d81a1434f727ef081294
Jon Newton <newt@hey.com>
2021-04-21 13:36:14 -0400

    Remove hello and add list function.
    List function returns items based on users prefix.  Does not return description yet, which requires a separate 'head' call for each S3 object.
    
    Will probably build a small side datastore for the metadata.

--- function/hello/hello.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-console.log('Loading function');
-
-exports.handler = async (_event, _context) => {
-  console.log('Invoking function');
-  return 'Hello there. Your random number is ' + Math.random();
-};
---
+++ function/list/list.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+const region = 'us-east-2';
+
+exports.handler = async (event) => {
+  const sub = event.sub || 'not-defined';
+  const mode = event.mode || 'not-defined';
+
+  console.log('sub', sub);
+  console.log('mode', mode);
+
+  const s3 = new AWS.S3({
+    apiVersion: '2006-03-01',
+    region: region
+  });
+
+  const params = {
+    Bucket: 'tapedeck-archives',
+    Prefix: sub
+  };
+
+  // try {
+  //   const versionResult = await new Promise((resolve, reject) => {
+  //     s3.listObjectVersions(params, (err, data) => {
+  //       console.log('err', err);
+  //       console.log('data', data);
+  //       if (err) {
+  //         reject(err);
+  //       }
+  //       else {
+  //         resolve(data);
+  //       }
+  //     });
+  //   });
+  // }
+  // catch (error) {
+  //   console.error(error);
+  // }
+
+  try {
+    const listResult = await new Promise((resolve, reject) => {
+      s3.listObjectsV2(params, (err, data) => {
+        console.log('err', err);
+        console.log('data', data);
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+
+    /*
+      
+      */
+    const response = {
+      mode: mode,
+      maxKeys: listResult.MaxKeys,
+      keyCount: listResult.KeyCount,
+      truncated: listResult.IsTruncated,
+      items: []
+    };
+
+    if (listResult.Contents) {
+      const baseUrl = `https://${listResult.Name}.s3.${region}.amazonaws.com`;
+      for (const item of listResult.Contents) {
+        response.items.push({
+          id: item.key,
+          url: `${baseUrl}/${item.Key}`,
+          desc: 'n/a', //TODO: populate
+          lastModified: item.LastModified,
+          size: item.Size
+        });
+      }
+    }
+
+    return response;
+  } catch (error) {
+    return error;
+  }
+};
