13cd475ebae910cd876735361d91a0c891c6780f
Jon Newton <newt@hey.com>
2021-04-23 23:17:09 -0400

    Rewrite function to accept SNS message.
    It only transfers the mp3 into S3 bucket based on data in the message.
    
    Later it will update DynamoDB as well.

--- function/archive/archive.js
+++ function/archive/archive.js
@@ -8,163 +8,77 @@
    see https://github.com/axios/axios/issues/1975 */
 const axios = require('axios').default;
 const stream = require('stream');
-const uuid = require('uuid');
 const AWS = require('aws-sdk');
-const HLS = require('parse-hls').default;
 
 const region = 'us-east-2';
 const bucket = 'tapedeck-archives';
 
 AWS.config.logger = console;
 
-function parseM3UFile(fileContents) {
-  return HLS.parse(fileContents).segments.map((segment) => {
-    return segment.properties.reduce(
-      (accum, prop) => {
-        if (
-          /* stop at first title */
-          accum.title === null &&
-          prop.type === 'TAG' &&
-          prop.tagName === 'EXTINF'
-        ) {
-          accum.title = prop.attributes.title;
-        }
+const s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  region: region
+});
 
-        return accum;
-      },
-      { uri: segment.uri, title: null }
-    );
-  });
-}
-
-const getMP3URLs = (playlistItems) => {
-  return playlistItems.filter((item) => item.uri.match(/\.mp3$/g));
-};
-
-const upload = async (desc, sub, resource, pass) => {
-  console.log(`[upload] processing ${resource.uri}`);
-
-  //TODO: better file name
-  const key = `${sub}/${uuid.v4()}.mp3`;
-
-  AWS.config.getCredentials(function (err) {
-    if (err) {
-      console.log('[upload] credentials not loaded', JSON.stringify(err));
-    } else {
-      console.log('[upload] Access key', AWS.config.credentials.accessKeyId);
-    }
-  });
-
-  const s3 = new AWS.S3({
-    apiVersion: '2006-03-01',
-    region: region
-  });
-
-  const params = {
-    Bucket: bucket,
-    Key: key,
-    Metadata: {
-      'playlist-description': desc
-    },
-    Body: pass
-  };
-
-  const upload = s3.upload(params).promise();
-  const data = await upload;
-  console.log('[upload] finished write to s3', data);
-};
-
-const badStatus = (msg) => {
-  console.log('calling badStatus');
-  return {
-    statusCode: 500,
-    body: JSON.stringify({ message: msg })
-  };
-};
-
-const goodStatus = (count) => {
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Content-Type',
-      'Access-Control-Allow-Methods': 'OPTIONS,GET'
-    },
-    body: JSON.stringify({ message: `# of items processed=${count}` })
-  };
+//reject promise when status != 200
+const validateStatus = (status) => {
+  return status === 200;
 };
 
 /*
-  Event be like
-   {
-    "url": "https://tapedeck-sample-files.s3.us-east-2.amazonaws.com/test.m3u",
-    "desc": "A test file",
-    "sub": "DnHdKa1qSEuN70gBffDXj"
-   } 
- */
-const handler = async (event, context) => {
-  console.log('[handler] starting', event, context);
+  event is from SNS.
+  Key payload is event.Records[i].Sns.Message which has format:
+  {
+    url: <string>,
+    pk:  <string>,
+    sk:  <string>
+  }
+*/
+const handler = async (event) => {
+  console.log('starting', event);
 
-  //Based on testing, I believe event is a proxy object
+  //Based on testing, I believe event is a JS proxy object
   //and that lambda will perform validation when you access
-  //properties
-  const url = event.url;
-  console.log('url is', url);
+  //properties.  If you access a property that doesn't exist
+  //then your lambda invocation will fail.
 
-  const desc = event.desc;
-  const sub = event.sub;
+  for (const r of event.Records) {
+    try {
+      const msg = JSON.parse(r.Sns.Message);
+      console.log('processing message', msg);
 
-  const maxFiles = 1;
-  try {
-    const res = await axios.get(url);
-    console.log('[handler] headers:', JSON.stringify(res.headers));
+      let mp3Response = await axios.get(msg.url, {
+        validateStatus,
+        responseType: 'stream'
+      });
 
-    const { status } = res;
-    const contentType = res.headers['content-type'];
+      console.log('got mp3 response');
 
-    if (status !== 200) {
-      const msg = `Failed to GET ${url}, status=${status}`;
-      console.log('[handler]', new Error(msg));
-      return badStatus(msg);
+      let pass = new stream.PassThrough();
+      mp3Response.data.pipe(pass);
+
+      const key = `${msg.pk}/${msg.sk}.mp3`;
+
+      const params = {
+        Bucket: bucket,
+        Key: key,
+        // Metadata: {
+        //   'playlist-description': desc
+        // },
+        Body: pass
+      };
+
+      console.log('write to s3', key);
+      const upload = s3.upload(params).promise();
+      const data = await upload;
+      console.log('finished write to s3', data);
+      return JSON.stringify({ status: 200 });
+    } catch (error) {
+      console.log('caught an error', error);
+      throw error;
     }
-    //Might need to support type aliases - http://help.dottoro.com/lapuadlp.php
-    else if (contentType !== 'audio/x-mpegurl') {
-      const msg = `Unsupported content-type for ${url}, content-type=${contentType}`;
-      console.log('[handler]', msg);
-      return badStatus(msg);
-    } else {
-      const filesToDownload = getMP3URLs(parseM3UFile(res.data));
-      console.log('[handler] files:', JSON.stringify(filesToDownload));
-
-      let count = 0;
-      for (const resource of filesToDownload) {
-        if (count++ > maxFiles) {
-          break;
-        }
-
-        let mp3Response = await axios.get(resource.uri, {
-          responseType: 'stream'
-        });
-
-        let pass = new stream.PassThrough();
-        mp3Response.data.pipe(pass);
-
-        await upload(desc, sub, resource, pass);
-        console.log('[handler]', 'upload await is done');
-      }
-
-      console.log('[handler]', 'calling goodStatus');
-      return goodStatus(count);
-    }
-  } catch (error) {
-    console.log('[handler] caught an error', error);
-    return badStatus(error.message);
   }
 };
 
 //THE entry point for lambda
 module.exports.handler = handler;
-
-//For unit tests
-module.exports.parseM3UFile = parseM3UFile;
-module.exports.getMP3URLs = getMP3URLs;
