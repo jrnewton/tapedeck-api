63b702b824ee24a99d452a8d2774027fa6acecce
Jon Newton <newt@hey.com>
2021-04-29 16:18:45 -0400

    Require the handler direct

--- function/request/test/request.e2e.js
+++ function/request/test/request.e2e.js
@@ -1,12 +1,12 @@
 'use strict';
 const fs = require('fs');
 const assert = require('assert');
-const request = require('../request.js');
+const handler = require('../request.js').handler;
 
 it('end to end test', async () => {
   const event = JSON.parse(fs.readFileSync('event.json', 'utf-8'));
   console.log('event', event);
-  const response = await request.handler(event);
+  const response = await handler(event);
   console.log('response', response);
   assert.strictEqual(response.statusCode, 200);
   const body = JSON.parse(response.body);
