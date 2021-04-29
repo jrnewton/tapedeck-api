befba48da3875768346354881bbb893a401684ce
Jon Newton <newt@hey.com>
2021-04-29 07:02:40 -0400

    Improve e2e test assertions

--- function/request/test/request.e2e.js
+++ function/request/test/request.e2e.js
@@ -1,21 +1,14 @@
 'use strict';
-
+const fs = require('fs');
 const assert = require('assert');
 const request = require('../request.js');
 
-it('provide a list of resources from an m3u playlist', async () => {
-  const event = {
-    url:
-      'https://tapedeck-sample-files.s3.us-east-2.amazonaws.com/test_medium.m3u',
-    desc: 'e2e test',
-    sub: 'd6ade639-bada-4ea2-9381-52c9d625cec2'
-  };
-
+it('end to end test', async () => {
+  const event = JSON.parse(fs.readFileSync('event.json', 'utf-8'));
   console.log('event', event);
-
   const response = await request.handler(event);
-
-  assert.notStrictEqual(response, null);
-
-  console.log('handler response', response);
+  console.log('response', response);
+  assert.strictEqual(response.statusCode, 200);
+  const body = JSON.parse(response.body);
+  assert.strictEqual(body.count, 2);
 });
