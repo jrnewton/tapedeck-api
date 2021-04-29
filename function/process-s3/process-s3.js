8efa9560ba2277bcbff936f729b51b1dd6462eaa
Jon Newton <newt@hey.com>
2021-04-28 23:26:17 -0400

    Fix bug in S3 metadata validation.  Keys are always lowercase.
    There is still a bug in updateItem. Will fix next.

--- function/process-s3/process-s3.js
+++ function/process-s3/process-s3.js
@@ -44,8 +44,10 @@ exports.handler = async (event) => {
 
       try {
         const { Metadata } = await s3.getObject(params).promise();
-        if (Metadata && Metadata.PK && Metadata.SK) {
-          const { PK, SK } = Metadata;
+        if (Metadata && Metadata.pk && Metadata.sk) {
+          //Amazon S3 stores user-defined metadata keys in *lowercase*
+          //https://docs.aws.amazon.com/AmazonS3/latest/userguide/UsingMetadata.html#object-metadata
+          const { pk, sk } = Metadata;
           const S3URL = `https://${Bucket}.s3.${awsRegion}.amazonaws.com/${Key}`;
           //https://tapedeck-archives.s3.us-east-2.amazonaws.com/d6ade639-bada-4ea2-9381-52c9d625cec2/1ebc2c3d-9972-4ce8-9fa2-b76b7babb44c.mp3
 
@@ -56,10 +58,10 @@ exports.handler = async (event) => {
             //prettier-ignore
             Key: {
               'PK': {
-                S: PK
+                S: pk
               },
               'SK': {
-                S: SK
+                S: sk
               }
             },
             //prettier-ignore
@@ -100,7 +102,10 @@ exports.handler = async (event) => {
 
           i++;
         } else {
-          console.error('Error in metadata', Metadata);
+          console.error(
+            'Error - metadata missing or required user-defined keys not present',
+            Metadata
+          );
         }
       } catch (error) {
         const message = `Error getting object ${Key} from bucket ${Bucket}`;
