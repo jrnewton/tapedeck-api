c1f1eb838a840a54ba921cc148de90782cb97b32
Jon Newton <newt@hey.com>
2021-05-02 21:05:09 -0400

    Make error handling specific to each failure.
    I had try/catch nested such that UpdateItem would also trigger catch on S3 GetObject.
    
    Fix some UpdateItem problems including attribute change for Status to ItemStatus.

--- function/process-s3/process-s3.js
+++ function/process-s3/process-s3.js
@@ -43,75 +43,79 @@ exports.handler = async (event) => {
         Key
       };
 
+      let Metadata = null;
+
       try {
-        const { Metadata } = await s3.getObject(params).promise();
-        if (Metadata && Metadata.pk && Metadata.sk) {
-          //Amazon S3 stores user-defined metadata keys in *lowercase*
-          //https://docs.aws.amazon.com/AmazonS3/latest/userguide/UsingMetadata.html#object-metadata
-          const { pk, sk } = Metadata;
-          const S3URL = `https://${Bucket}.s3.${awsRegion}.amazonaws.com/${Key}`;
-          //https://tapedeck-archives.s3.us-east-2.amazonaws.com/d6ade639-bada-4ea2-9381-52c9d625cec2/1ebc2c3d-9972-4ce8-9fa2-b76b7babb44c.mp3
-
-          const now = new Date().toISOString();
-
-          const itemParams = {
-            TableName: table,
-            //prettier-ignore
-            Key: {
-              'PK': {
-                S: pk
-              },
-              'SK': {
-                S: sk
-              }
-            },
-            //prettier-ignore
-            UpdateExpression: 'SET LastModified = :modDate, Status = :status, LastDownloadDate = :downloadDate, DownloadCount = DownloadCount + :downloadCount, S3URL = :url, S3SIZE = :size',
-            //prettier-ignore
-            ExpressionAttributeValues: {
-              ':modDate': {
-                S: now
-              },
-              ':status': { 
-                S: itemStatus
-              },
-              ':downloadDate': { 
-                S: now
-              },
-              ':downloadCount': {
-                N: '1'
-              },
-              ':url': {
-                S:S3URL
-              },
-              ':size': {
-                N: objectSize
-              }
-            }
-          };
-
-          try {
-            console.log('UpdateItem params', itemParams);
-            const putItemResponse = await ddbClient.send(
-              new UpdateItemCommand(itemParams)
-            );
-            console.log('UpdateItem response', putItemResponse);
-          } catch (error) {
-            console.error('UpdateItem error', error);
-            throw error;
-          }
-
-          i++;
-        } else {
-          console.error(
-            'Error - metadata missing or required user-defined keys not present',
-            Metadata
-          );
-        }
+        const s3Response = await s3.getObject(params).promise();
+        Metadata = s3Response.Metadata;
       } catch (error) {
         const message = `Error getting object ${Key} from bucket ${Bucket}`;
         console.error(message, error);
         throw new Error(message);
+      }
+
+      if (Metadata && Metadata.pk && Metadata.sk) {
+        //Amazon S3 stores user-defined metadata keys in *lowercase*
+        //https://docs.aws.amazon.com/AmazonS3/latest/userguide/UsingMetadata.html#object-metadata
+        const { pk, sk } = Metadata;
+        const S3URL = `https://${Bucket}.s3.${awsRegion}.amazonaws.com/${Key}`;
+        //https://tapedeck-archives.s3.us-east-2.amazonaws.com/d6ade639-bada-4ea2-9381-52c9d625cec2/1ebc2c3d-9972-4ce8-9fa2-b76b7babb44c.mp3
+
+        const now = new Date().toISOString();
+
+        const itemParams = {
+          TableName: table,
+          //prettier-ignore
+          Key: {
+            'PK': {
+              S: pk
+            },
+            'SK': {
+              S: sk
+            }
+          },
+          //prettier-ignore
+          UpdateExpression: 'SET LastModified = :modDate, ItemStatus = :itemStatus, LastDownloadDate = :downloadDate, DownloadCount = DownloadCount + :downloadCount, S3URL = :url, S3Size = :size',
+          //prettier-ignore
+          ExpressionAttributeValues: {
+            ':modDate': {
+              S: now
+            },
+            ':itemStatus': { 
+              S: itemStatus
+            },
+            ':downloadDate': { 
+              S: now
+            },
+            ':downloadCount': {
+              N: '1'
+            },
+            ':url': {
+              S: S3URL
+            },
+            ':size': {
+              N: objectSize+''
+            }
+          }
+        };
+
+        try {
+          console.log('UpdateItem params', itemParams);
+          const putItemResponse = await ddbClient.send(
+            new UpdateItemCommand(itemParams)
+          );
+          console.log('UpdateItem response', putItemResponse);
+        } catch (error) {
+          console.error('UpdateItem error', error);
+          throw error;
+        }
+
+        i++;
+      } else {
+        console.error(
+          'Error - metadata missing or required user-defined keys not present',
+          Metadata
+        );
       }
     } else {
       console.log('skipping event type', eventName);
